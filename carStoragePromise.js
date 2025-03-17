e10805a6b1a7e397da4dd0791f232bf9a8dafd13
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-17 13:40:42 +0200

    first commit - add start files

---
+++ carStoragePromise.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import fs from "node:fs/promises";
+
+const storageFilePath = '.cars.json';
+
+export default function search(key, value) {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            const data = await fs.readFile(storageFilePath, 'utf8');
+            const cars = JSON.parse(data);
+            if (arguments.length < 2) {
+                resolve(cars);
+            } else {
+                resolve(cars.filter(car => car[key] === value));
+            }
+
+        } catch (error) {
+            reject([]);
+        }
+
+    })
+}
---
+++ test/carstoragePromise.test.js
@@ -0,0 +1,6 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);

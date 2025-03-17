31fe4960e1b07721f421a59b147ff4b29ee04a9a
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-17 13:44:17 +0200

    add 1st test

--- test/carstoragePromise.test.js
+++ test/carstoragePromise.test.js
@@ -6,3 +6,10 @@ const expect = chai.expect;
 
 chai.use(chaiAsPromised);
 
+describe('Testing with key license', () => {
+    const result = [{ "model": "Bored T-model", "license": "ABC-1" }];
+
+    it(`resolves to ${result}`, function () {
+        return expect(search('license', 'ABC-1')).to.eventually.deep.equal(result);
+    });
+});

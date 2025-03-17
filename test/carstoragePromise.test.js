b4a68fe7f87002e0b5c59d8f85c5d35f9c9a6a3c
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-03-17 14:13:22 +0200

    add search by model test

--- test/carstoragePromise.test.js
+++ test/carstoragePromise.test.js
@@ -17,3 +17,17 @@ describe('Testing with key license', function () {
         return expect(search('license', 'x')).to.eventually.to.be.empty;
     });
 });
+
+describe('search by model', function () {
+    const result = [{ "model": "Bored T-model", "license": "ABC-1" }];
+    it('model: "Bored T-model"', function () {
+        return expect(search('model', 'Bored T-model')).to.eventually.deep.equal(result)
+    });
+
+    const testValues = [{ "model": "Turbo V8", "license": "DEF-4" }, { "model": "Speedster X", "license": "XYZ-2" }];
+    testValues.forEach(function (value) {
+        it(`model: ${value.model}`, function () {
+            return expect(search('model', value.model)).to.eventually.deep.equal([value]);
+        });
+    });
+});
